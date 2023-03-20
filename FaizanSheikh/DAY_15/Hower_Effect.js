83ba805ead1d113fe8b7419fc4d3fa9b77e01e80
Faizan_Sheikh <faizansheikh2002as786ji@gmail.com>
2023-03-20 19:20:15 +0530

    faizan_DAY_14/15

---
+++ FaizanSheikh/DAY_15/Hower_Effect.js
@@ -0,0 +1,21 @@
+function displaycolor(colourname){
+
+    if(colourname== "red"){
+
+        document.getElementById('displaycolour').style.backgroundColor=colourName
+
+    }else if(colourname== "Green"){
+
+        document.getElementById('displaycolour').style.backgroundColor=colourName
+
+    }else{
+        document.getElementById('displaycolour').style.backgroundColor=colourName
+
+    }
+
+
+
+    //var value = document.getElementsByClassName("red").inner
+
+    //alert(value)
+}
