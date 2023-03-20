566f820453bc0d5c0a4efcb375b1099eb97cfd38
nikitazode <zodenkita@gmail.com>
2023-03-20 01:57:47 -0700

    'Assignment_Traffic-Signal'

---
+++ nikita_zode/day16/trafficSignal.js
@@ -0,0 +1,19 @@
+function makeLight(Light1){
+  let stopLight=document.getElementById("red");
+  let slowLight=document.getElementById("yellow");
+  let goliLight=document.getElementById("green");
+
+  if(Light1=="red"){
+    stopLight.style.backgroundColor = "red";
+    slowLight.style.backgroundColor = "";
+    goliLight.style.backgroundColor = "";
+  }else if(Light1=="yellow"){
+    stopLight.style.backgroundColor = "";
+    slowLight.style.backgroundColor = "yellow";
+    goliLight.style.backgroundColor = "";
+  }if(Light1=="green"){
+    stopLight.style.backgroundColor = "";
+    slowLight.style.backgroundColor = "";
+    goliLight.style.backgroundColor = "green";
+  }
+}
