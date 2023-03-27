e0ffb03e3d96f52d765a3f8b7b9b7617693865b7
pranjaliawale22 <pranjaliawale22@gmail.com>
2023-03-27 19:07:22 +0530

    boxs animation

---
+++ pranjaliawale/day16/day16.js
@@ -0,0 +1,14 @@
+function arrayIntro(){
+    var a = [11,22,333,433,111,555,444,555,"Abhishek",777,889,["aa","bb","cc"],222,444,666,5]
+
+    console.log(typeof(a))
+    for(var i=0;i<a.length;i++){
+        if(typeof(a[i])=="object"){
+            for(var j=0;j<a[i].length;j++){
+                console.log(a[i][j])
+            }
+        }else{
+            console.log(a[i])
+      }
+    }
+}
---
+++ pranjaliawale/day17/day17.js
@@ -0,0 +1,15 @@
+function red(){
+    document.getElementById("red").style.backgroundColor="red";
+    document.getElementById("yellow").style.backgroundColor="";
+    document.getElementById("green").style.backgroundColor="";
+}
+function yellow(){
+    document.getElementById("red").style.backgroundColor="";
+    document.getElementById("yellow").style.backgroundColor="yellow";
+    document.getElementById("green").style.backgroundColor="";
+}
+function green(){
+    document.getElementById("red").style.backgroundColor="";
+    document.getElementById("yellow").style.backgroundColor="";
+    document.getElementById("green").style.backgroundColor="green";
+}
