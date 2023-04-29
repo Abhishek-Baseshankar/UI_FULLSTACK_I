51eca41db89e345b06734ab73be7d63e21ed4bc2
Aditya Nikhate <adityanikhate02@gmail.com>
2023-04-29 21:27:15 +0530

    This is Aditya Nikhate Major Project 1

---
+++ AdityaNikhate/MajorProject1/script.js
@@ -0,0 +1,7 @@
+// import https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js;
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js');
+document.head.appendChild(jQueryScript);
+function submit(){
+    $('#loginsection').hide();
+}
